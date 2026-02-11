d7349aee5396a71561d3d1490c5ce50d7bffcf8e
LegolasGreenleaf1999 <ashwincsanthosh@gmail.com>
2026-02-11 11:01:56 +0530

    test

--- proj/settings.py
+++ proj/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+# deploy test
 import os
 from pathlib import Path
 from dotenv import load_dotenv
